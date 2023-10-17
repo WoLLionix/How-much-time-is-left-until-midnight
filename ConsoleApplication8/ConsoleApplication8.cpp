7e420acb89453358a667ba28191d4ced59f767e5
Bohdan <Bohdan@DESKTOP-PIR5C7D>
2023-10-17 14:13:18 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApplication8/ConsoleApplication8.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+int main()
+{
+    int inputSecond;
+    std::cout << "Enter the time: ";
+    std::cin >> inputSecond;
+
+    if (inputSecond < 0)
+    {
+        std::cout << "Error. Time cannot be less than zero";
+    }
+
+    int second, minute, hour, day;
+    day = inputSecond / 86400;
+    inputSecond %= 86400;
+
+    hour = inputSecond / 3600;  
+    inputSecond %= 3600;
+
+    minute = inputSecond / 60;
+    second = inputSecond % 60;
+
+    std::cout << "Days: " << day << ", Hours: " << hour << ", Minutes: " << minute << ", Seconds: " << second << std::endl;
+
+    hour = 24 - hour;
+    minute = 60 - minute;
+    second = 60 - second;
+
+    std::cout << std::endl << "How much time is left until midnight ?\n";
+    std::cout <<"Hours: " << hour << ", Minutes: " << minute << ", Seconds: " << second;
+}
